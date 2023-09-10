9e6134b69a60db92fcf95c78d77e7f62970c8dab
ikonilovelive <140684321+mykonshshhdhdhdh@users.noreply.github.com>
2023-09-10 12:49:38 +0800

    Add files via upload

---
+++ happy.js
@@ -0,0 +1,32 @@
+var d = new Date();
+var x = d.getUTCDate();
+var y = d.getMonth();
+// 0=1月，1=2月
+
+if(x==18&&y==8){
+document.getElementById("tsb").innerHTML="✎...祝站长生日快乐！！！";
+}
+if(x==14&&y==9){
+document.getElementById("tsb").innerHTML="✎...祝五月七日茴香学姐生日快乐！！！";
+} 
+if(x==21&&y==9){
+document.getElementById("tsb").innerHTML="✎...祝绘里学姐生日快乐！！！";
+} 
+if(x==11&&y==10){
+document.getElementById("tsb").innerHTML="✎...祝阿梓喵生日快乐！！！";
+} 
+if(x==27&&y==10){
+document.getElementById("tsb").innerHTML="✎...祝平泽唯生日快乐！！！";
+} 
+if(x==26&&y==12){
+document.getElementById("tsb").innerHTML="✎...祝真锅和生日快乐！！！";
+}
+if(x==15&&y==0){
+document.getElementById("tsb").innerHTML="✎...祝mio生日快乐！！！";
+}
+if(x==22&&y==1){
+document.getElementById("tsb").innerHTML="✎...祝平泽优生日快乐！！！";
+} 
+if(x==19&&y==3){
+document.getElementById("tsb").innerHTML="✎...祝真姬生日快乐！！！";
+} 
